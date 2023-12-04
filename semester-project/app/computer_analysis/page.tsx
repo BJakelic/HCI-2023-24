e46867505808f65e1e20a44673a977bce9e969d1
lmuniv00 <lmuniv00@fesb.hr>
2023-12-04 23:02:47 +0100

    Updated files for semester project

---
+++ semester-project/app/computer_analysis/page.tsx
@@ -0,0 +1,9 @@
+function Computer_Analysis() {
+    return (
+        <main className="flex justify-center text-4xl p-14">
+            <h1>Computer Analysis</h1>
+        </main>  
+    );
+}
+
+export default Computer_Analysis;
