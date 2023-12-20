2fde6d9c6ee190a7ca11535bfc56b3d32f085faa
lmuniv00 <lmuniv00@fesb.hr>
2023-12-20 13:40:55 +0100

    Updated button link

--- semester-project/components/DropdownMenu.tsx
+++ semester-project/components/DropdownMenu.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Fragment } from 'react'
+import Link from "next/link";
 import { Menu } from '@headlessui/react'
 import { Page } from "@/components/Navbar";
 
@@ -8,24 +9,24 @@ export default function MyMenu({ pages }: { pages: Page[] }) {
   return (
     <Menu>
       <div className="flex items-center justify-center">
-        <a href="#" className="block md:hidden">
+        <Link href="#" className="block md:hidden">
           <Menu.Button className="uppercase whitespace-nowrap font-roboto-condensed font-bold text-base px-5 py-3 rounded-xl text-brand-special-200 bg-brand-blue-100 hover:bg-brand-blue-300 border-2 border-brand-special-200">
             MENU
           </Menu.Button>
-        </a>
+        </Link>
       </div>
       <Menu.Items className="grid grid-cols-1 gap-y-2 mt-2">
         {pages.map((page) => (
           <Menu.Item key={page.href} as={Fragment}>
             {({ active }) => (
-              <a
+              <Link
                 href={page.href}
                 className={`block uppercase whitespace-nowrap font-roboto-condensed font-bold text-base px-5 py-3 rounded-xl text-brand-purple-900 bg-brand-blue-200 hover:bg-brand-blue-300 border-2 border-brand-purple-900 ${
                   active ? 'text-white' : 'text-brand-purple-900'
                 }`}
               >
                 {page.title}
-              </a>
+              </Link>
             )}
           </Menu.Item>
         ))}
