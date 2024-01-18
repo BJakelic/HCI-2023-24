201770a98d504d307c45333738d9438681efaac6
BJakelic <bjakel@fesb.hr>
2024-01-18 11:06:01 +0100

    Page currently not working (feedback)

--- semester-project/app/game_requirements/page.tsx
+++
@@ -1,9 +0,0 @@
-function Game_Requirements() {
-    return (
-        <main className="flex justify-center text-4xl p-14">
-            <h1>Game Requirements</h1>
-        </main>  
-    );
-}
-
-export default Game_Requirements;
