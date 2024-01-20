a5cdd6cb25713f08f7214c8cc70d3d39eead1478
lmuniv00 <lmuniv00@fesb.hr>
2024-01-20 16:21:46 +0100

    Added pagination to Feedback page

--- semester-project/app/feedback/page.tsx
+++ semester-project/app/feedback/page.tsx
@@ -1,38 +1,112 @@
+"use client";
+
 import Link from "next/link";
-import { useRouter } from "next/router";
+import clsx from "clsx";
 
 export interface Post {
-    userId: number;
-    id: number;
-    title: string;
-    body: string;
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface Pagination {
+  limit: number;
+  page: number;
 }
 
 const BASE_API_URL = "https://jsonplaceholder.typicode.com";
 
-const getPosts = async (): Promise<Post[]> => {
-  const data = await fetch(`${BASE_API_URL}/posts`);
+const getPosts = async (
+  pagination: Pagination = {
+    limit: 9999,
+    page: 1,
+  }
+): Promise<Post[]> => {
+  const data = await fetch(
+    `${BASE_API_URL}/posts?_limit=${pagination.limit}&_page=${pagination.page}`
+  );
   return data.json();
 };
 
-export default function CommunityInsights({ posts }: { posts: Post[] }) {
-  const router = useRouter();
+const getTotalPosts = async (): Promise<number> => {
+  const response = await fetch(`${BASE_API_URL}/posts?_limit=1`, {
+    method: "HEAD",
+  });
+  // get x-total-count header
+  return parseInt(response.headers.get("x-total-count") || "1", 10);
+};
 
-  // Get the requested post ID from the router
-  const requestedPostId = parseInt(router.query.postId as string, 10);
+export default async function Feedback({
+  searchParams,
+}: {
+  searchParams: Record<string, string | string[] | undefined>;
+}) {
+  const { _limit, _page } = searchParams;
+  var [pageSize, page] = [_limit, _page].map(Number);
+  const totalPosts = await getTotalPosts();
+  const totalPages = Math.ceil(totalPosts / 10);
 
-  // Check if the requested post exists
-  const requestedPost = posts.find((post) => post.id === requestedPostId);
+  if(isNaN(page))
+    page = 1
 
-  // If the post doesn't exist, redirect to 404
-  if (!requestedPost && !Number.isNaN(requestedPostId)) {
-    router.push("/not-found.tsx");
-    return null; // Return null to avoid rendering the current page
-  }
+  const posts = await getPosts({
+    limit: 10,
+    page: page,
+  });
 
   return (
     <main className="flex flex-col items-center min-h-screen max-w-5xl m-auto p-10">
       <h1 className="text-3xl font-bold p-10">Feedback</h1>
+        <div className="flex items-baseline gap-8 pb-10">
+          <div>
+            Page {page} of {totalPages}
+          </div>
+          <div className="flex gap-4">
+            <Link
+              href={{
+                pathname: "/feedback",
+                query: { _page: 1, _limit: pageSize },
+              }}
+              className="rounded border bg-gray-100 px-3 py-1 text-gray-800"
+            >
+              First
+            </Link>
+            <Link
+              href={{
+                pathname: "/feedback",
+                query: { _page: page > 1 ? page - 1 : 1, _limit: pageSize },
+              }}
+              className={clsx(
+                "rounded border bg-gray-100 px-3 py-1 text-gray-800",
+                page === 1 && "pointer-events-none opacity-50"
+              )}
+            >
+              Previous
+            </Link>
+            <Link
+              href={{
+                pathname: "/feedback",
+                query: { _page: page + 1, _limit: pageSize },
+              }}
+              className={clsx(
+                "rounded border bg-gray-100 px-3 py-1 text-gray-800",
+                page === totalPages && "pointer-events-none opacity-50"
+              )}
+            >
+              Next
+            </Link>
+            <Link
+              href={{
+                pathname: "/feedback",
+                query: { _page: totalPages, _limit: pageSize },
+              }}
+              className="rounded border bg-gray-100 px-3 py-1 text-gray-800"
+            >
+              Last
+            </Link>
+          </div>
+        </div>
       <ul className="flex flex-col gap-8">
         {posts.map((post) => (
           <li key={post.id}>
@@ -46,16 +120,4 @@ export default function CommunityInsights({ posts }: { posts: Post[] }) {
       </ul>
     </main>
   );
-}
-
-// Fetch the posts during server-side rendering
-export async function getServerSideProps() {
-  const allPosts = await getPosts();
-  const posts = allPosts.slice(0, 100);
-
-  return {
-    props: {
-      posts,
-    },
-  };
 }
